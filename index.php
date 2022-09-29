68ef60aee2d26ce797e673ad28b7b5d1ed599515
salut <killian.portier@yahoo.fr>
2022-09-29 14:55:49 +0200

    Structure projet

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+
+    <?php
+        include('header.php')
+    ?>
+    <main>
+
+    <section>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae ipsum adipisci cum expedita, aliquam reiciendis. Error, et eaque nemo perferendis natus eveniet beatae voluptates omnis ipsa modi quod incidunt? Numquam?
+    </section>
+
+    <section>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, at hic ipsum corporis beatae voluptate natus placeat libero autem voluptatibus voluptas ad reprehenderit iure eligendi quaerat nihil incidunt quia ducimus!
+    </section>
+
+    <?php
+        include('main.php')
+    ?>
+    </main>
+
+    <?php
+        include('footer.php')
+    ?>
+</body>
+</html>
